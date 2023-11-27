07a133da2b12142072038ce369af320f1ec8ac94
xydche <cherhe@gmail.com>
2023-11-27 20:51:30 +0800

    优化diy packages

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,3 +18,5 @@
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 git clone https://github.com/xydche/luci-theme-design.git package/luci-theme-design
+git clone https://github.com/kenzok78/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone https://github.com/sbwml/luci-app-mosdns.git -b v5 package/mosdns
