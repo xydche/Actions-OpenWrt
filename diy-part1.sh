e6563e7ff191167ef70fc090a57321ddc3f226c0
xydche <cherhe@gmail.com>
2024-11-01 12:56:14 +0800

    优化软件包

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,8 +18,9 @@
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 # echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
-git clone https://github.com/xydche/luci-theme-design.git package/luci-theme-design
-git clone https://github.com/kenzok78/luci-app-adguardhome.git package/luci-app-adguardhome
-git clone https://github.com/rufengsuixing/luci-app-onliner package/onliner
+# git clone https://github.com/xydche/luci-theme-design.git package/luci-theme-design
+# git clone https://github.com/kenzok78/luci-app-adguardhome.git package/luci-app-adguardhome
+# git clone https://github.com/rufengsuixing/luci-app-onliner package/onliner
 # git clone https://github.com/sbwml/luci-app-mosdns.git -b v5 package/mosdns
 # git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+git clone https://github.com/tty228/luci-app-wechatpush package/luci-app-wechatpush
